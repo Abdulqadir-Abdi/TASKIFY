3798d2f0c5b0669ea02d6eff18b8668d1e6aba11
Abdulqadir-Abdi <apdallec@gmail.com>
2024-11-04 18:27:04 +0300

    try

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,3 +1,7 @@
+const { readTasksFromFile} = require("../utils/fileHandler");
+
 exports.getTasks = (req, res) => {
-   
+    const tasks = readTasksFromFile();
+    res.writeHead(200, { 'content-type': 'application/json'})
+    res.end(JSON.stringify(tasks))
 }
