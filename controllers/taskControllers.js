5aca231efb28231ba8c22dff25708832cb398b86
Abdulqadir-Abdi <apdallec@gmail.com>
2024-11-06 09:58:00 +0300

    try

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -104,5 +104,12 @@ exports.deleteTask = (req, res) => {
         }))
         return;
     }
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    writeTasksToFile(updatedTasks);
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(JSON.stringify({
+        message: 'Task successfully deleted'
+    }));
 }
+ 
 
